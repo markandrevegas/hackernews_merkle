00dceeecab8888cb4d70b98d4544e2087d7c9a8b
markandrevegas <markandrevega@me.com>
2025-12-04 21:07:42 +0100

    basic layout and styles

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -15,9 +15,14 @@ const today = new Date().toLocaleDateString('en-US', {
 			<h1>{{ message }}</h1>
 		</div>
 		<div class="app-content">
-      <p v-for="i in 20" :key="i">
-        Dummy content line {{ i }}
-      </p>
+      <div v-for="i in 10" :key="i" class="card">
+				<div style="display:flex; flex-direction:column; gap:8px; flex:1">
+					<h2>Story Title {{ i }}</h2>
+					<p>Lorem ipsum dolor, sit amet consectetur</p>
+				</div>
+				
+        <img :src="`https://picsum.photos/seed/${i}/80/64`" alt="">
+      </div>
     </div>
     <div class="app-footer">
       <p>End of content</p>
